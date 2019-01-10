7e156a74dd3a47670a263dbfe5dbc82e058c62ff
hujiande <593595854@qq.com>
2019-01-10 09:52:15 +0800

    modify

---
+++ springcloudmaw/src/main/java/com/hjd/springcloudmaw/SpringcloudmawApplication.java
@@ -0,0 +1,14 @@
+package com.hjd.springcloudmaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudmawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudmawApplication.class, args);
+	}
+
+}
+
