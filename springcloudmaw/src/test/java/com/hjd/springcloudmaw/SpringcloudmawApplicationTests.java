83cc9a939927c42d0d4ce1d9fce3f6c817a1fb6a
hujiande <593595854@qq.com>
2019-01-10 10:04:58 +0800

    modify

---
+++ springcloudmaw/src/test/java/com/hjd/springcloudmaw/SpringcloudmawApplicationTests.java
@@ -0,0 +1,17 @@
+package com.hjd.springcloudmaw;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringcloudmawApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
