04350037096d1096368821b4f7587b82e0dab0c6
hujiande <593595854@qq.com>
2019-01-10 10:30:20 +0800

    modify

--- eureka-server/src/main/java/com/hjd/springcloudmaw/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/hjd/springcloudmaw/eureka/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.springcloud.eureka;
+package com.hjd.springcloudmaw.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
